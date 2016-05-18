79eb21084e9c72f84e282295312452c158c3495f
Tyler Long <tyler4long@gmail.com>
2016-05-18 16:45:19 +0800

    Done

---
+++ RC.Xamarin.iOS.Subscription.Demo/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RC.Xamarin.iOS.Subscription.Demo
+{
+	public class Config
+	{
+		public const string AppKey = "";
+		public const string AppSecret = "";
+		public const string Server = "https://platform.devtest.ringcentral.com";
+		public const string Username = "";
+		public const string Extension = "";
+		public const string Password = "";
+		public const string Receiver = "";
+	}
+}
--- RC.Xamarin.iOS.Subscription.Demo/ViewController.cs
+++ RC.Xamarin.iOS.Subscription.Demo/ViewController.cs
@@ -1,5 +1,10 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json;
+using RingCentral.SDK;
+using RingCentral.SDK.Http;
+using RingCentral.Subscription;
 using UIKit;
 
 namespace RC.Xamarin.iOS.Subscription.Demo
@@ -14,6 +19,29 @@ namespace RC.Xamarin.iOS.Subscription.Demo
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			var sdk = new SDK (Config.AppKey, Config.AppSecret, Config.Server, "Test App", "1.0.0");
+			var platform = sdk.GetPlatform ();
+			platform.Authorize (Config.Username, Config.Extension, Config.Password, false);
+
+			var sub = new SubscriptionServiceImplementation() { _platform = platform };
+			sub.AddEvent("/restapi/v1.0/account/~/extension/~/message-store");
+			sub.Subscribe((message) => {
+				Console.WriteLine("Event Received");
+			}, null, null);
+
+			var dict = new Dictionary<string, dynamic> {
+				{ "text", "hello world" },
+				{ "from", new Dictionary<string, string> { { "phoneNumber", Config.Username} } },
+				{ "to", new Dictionary<string, string>[] { new Dictionary<string, string> { { "phoneNumber", Config.Receiver } } } },
+			};
+			var request = new Request("/restapi/v1.0/account/~/extension/~/sms", JsonConvert.SerializeObject(dict));
+			for(var i=0; i < 10; i++)
+			{
+				platform.Post(request);
+				Thread.Sleep (30000);
+			}
+			sub.Remove();
 		}
 
 		public override void DidReceiveMemoryWarning ()
@@ -23,4 +51,3 @@ namespace RC.Xamarin.iOS.Subscription.Demo
 		}
 	}
 }
-
